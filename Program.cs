dcfb3023a3ab6769edb620e9ef8e51ff1f423724
Gustavo <gustavomachadochagas@gmail.com>
2024-10-28 12:33:56 -0300

    commit inicial

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿string minhaString = "Olá, mundo! Este é um texto de teste com a letra a.";
+AnalisadorDeString.ContarAEmString(minhaString);
+
+public class AnalisadorDeString
+{
+    public static void ContarAEmString(string entrada)
+    {
+        int contador = 0;
+        foreach (char c in entrada)
+        {
+            if (c == 'a' || c == 'A' || c == 'á')
+            {
+                contador++;
+            }
+        }
+
+        Console.WriteLine($"A letra 'a' (incluindo acentuada) aparece {contador} vezes na string: '{entrada}'");
+    }
+}
